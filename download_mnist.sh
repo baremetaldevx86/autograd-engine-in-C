4db5eff9c6f967985a1770e210b4b449ae598b2e
Vedant Chauhan <123994504+baremetaldevx86@users.noreply.github.com>
2026-02-08 20:51:35 +0530

    Delete download_mnist.sh

--- download_mnist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p data
-cd data
-wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
-gunzip *.gz
-cd ..
